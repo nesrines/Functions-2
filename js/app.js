d86c3eaf1b900c678936b94cc42fd8ee5b00a695
nesrines <nasrinfa@code.edu.az>
2023-06-10 00:10:03 +0400

    third

--- js/app.js
+++ js/app.js
@@ -1,7 +1,11 @@
 document.getElementById("Calculate").onclick = () => {
-    let amount = parseInt(document.getElementById("Amount").value);
+    let amount = parseFloat(document.getElementById("Amount").value);
     let period = parseInt(document.getElementById("Period").value);
     let percent = parseFloat(document.getElementById("Percent").value);
-    document.getElementById("Monthly").innerHTML = ((amount + amount * percent / 100) / period).toFixed(2);
-    document.getElementById("Total").innerHTML = amount + amount * percent / 100;
+    if (!isNaN(amount) && !isNaN(period) && !isNaN(percent)) {
+        let total = (amount + amount * percent / 100).toFixed(2);
+        document.getElementById("Monthly").innerHTML = `${(total / period).toFixed(2)} AZN`;
+        document.getElementById("Total").innerHTML =  `${total} AZN`;
+        document.getElementById("Order").style.display = "inline-block";
+    }
 }
